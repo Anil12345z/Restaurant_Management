b35dd127364cbcd341ff36ee42252365e955b411
Anil chahar <84167081+Anil12345z@users.noreply.github.com>
2024-04-06 22:06:49 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:chaharanil568@gmail.com">chaharanil568@gmail.com</a>
+         <a href="mailto:sharmarahul2023@gmail.com">sharmarahul2023@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+        <p><p>7am to 10pm</p> monday to saturday</p> 
+      </div>
+
+      <div class="box">
+         
+         <h3>our address</h3>
+         <a href="#">Roorkee, india - 247667</a>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3458.491891608541!2d77.8376160651119!3d29.90773808193218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390eb4294403421f%3A0xf20e96269fa53f93!2sRIT%20Roorkee%20Engineering%20College%20in%20Uttarakhand!5e0!3m2!1sen!2sin!4v1685283683795!5m2!1sen!2sin" width="200" height="165" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:8462931560">8462931560</a>
+         <a href="tel:8462931560">8529488194</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Anil chahar</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
